f2adb53241fa869be3cefd283088a95974727b52
Tapasv <tapasvkaushal@gmail.com>
2026-02-09 10:50:40 +0530

    Api

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8000/api';
+const API_URL = 'https://devscribe-backend-a4ot.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_URL,

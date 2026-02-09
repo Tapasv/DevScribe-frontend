51b0bd46f99b8b58ac1ba115617cf0c0696eaa77
Tapasv <tapasvkaushal@gmail.com>
2026-02-09 10:19:07 +0530

    App

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/header';
-import Footer from './components/Footer';
+import Footer from './components/footer';
 import Home from './pages/Home';
 import PostDetail from './pages/PostDetail';
 import CategoryPage from './pages/CategoryPage';
